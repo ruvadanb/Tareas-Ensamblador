1ff55511255ae67841becdc62c9c3b621a8724c0
Daniel Ruvalcaba <ie715472@iteso.mx>
2019-09-22 18:42:55 -0500

    Tarea 4 ejercicio 4 archivo ensamblador

--- Tarea4-4_archivo_Ensamblador.asm
+++ Tarea4-4_archivo_Ensamblador.asm
@@ -2,12 +2,13 @@
 
 .text
 	main:
-	li $s0, 6 #n
+	li $s0, 3 #n
 	li $s1, 1 #i
 	li $s2, 1 #variable resultado
 	li $t2, 1 #constante
 	li $t3, 2 #base 2
 	addi $s0, $s0, 1
+	jal for
 	
 	for:
 	slt $t0, $s1, $s0 # si i<n t0=1
@@ -17,6 +18,6 @@
 	multiplicacion:
 	addi $s1, $s1, 1 #aumentamos i++
 	mul $s2, $t3, $s2 #multiplicacmos 2*resultado
-	j for #regresamos al for
+	jr $ra #regresamos al for
 	
 	exit:
