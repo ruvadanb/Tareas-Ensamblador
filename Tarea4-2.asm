8e4a3a566d78caa6a12c20877964d27bbb252075
Daniel Ruvalcaba <ie715472@iteso.mx>
2019-09-22 16:15:13 -0500

    Tarea 4 ejercicio 2

--- Tarea4-2.asm
+++ Tarea4-2.asm
@@ -1,18 +1,18 @@
 .data
-	li $s0, 0 #selector
-	li $s1, 5 #constante a
-	li $s2, 3 #constante b
-	li $s3, 0 #constante
 
 .text
 
 	main:
+	li $s0, 0 #selector
+	li $s1, 5 #constante a
+	li $s2, 3 #constante b
+	li $s3, 0 #constante
 	li $t0, 1
 	li $t1, 2
 	li $t2, 3
 	beq $s0, $t0, suma
 	beq $s0, $t1, resta
-	beq $s0, $t3, multiplicacion
+	beq $s0, $t2, multiplicacion
 	j Andbitwise
 	
 	suma:
