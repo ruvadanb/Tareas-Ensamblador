71bf5bc4b689323b22380d52dd37b682a59ce741
Daniel Ruvalcaba <ie715472@iteso.mx>
2019-09-22 15:44:09 -0500

    Tarea 4 ejercicio 1

--- Tarea4-1.asm
+++ Tarea4-1.asm
@@ -5,34 +5,37 @@ Result: .word 0 #s3
 
 .text
 main:
-	la $s1, Vector1
+	la $s1, Vector1 #guardamos la direccion del vector
 	la $s2, Vector2
 	la $s3, Result
-	li $a0, 3 # k=3
+	li $a0, 4 # k-1=3
 	li $t0, 0 # i=0
 	li $t1, 0 #temporal register
 	li $t2, 0 #temporal register
 	li $t5, 0 # Acc
-	jal for #Llamamos la funcion
+	j for #Llamamos la funcion
 
 for:
+	subi $a0, $a0,1
 	slti $t6, $a0, 1
-	beq $t6, $zero, mulAndAccumulate
-	j exit
+	beq $t6, $zero, multAndAccumulate
+	j Exit
 	
 multAndAccumulate:
 	sll $t1, $t0, 2 #shift left i
-	lw $t2, $t1
+	sll $t2, $t0, 2
 	add $t1, $t1, $s1 #Adding an offset
 	add $t2, $t2, $s2 #Adding offset
 	lw $t3, 0($t1) #Leemos el 1 y lo guardamos en t3
 	lw $t4, 0($t2) #Leemos el 6 y lo guardamos en t4
-	mult $t3, $t4 #t3=mulres
-	jal Accumulate
+	mul $t7, $t3, $t4 #t7=mulres
+	jal Accumulate #llamamos a la fucnion que suma
+	addi $t0, $t0, 1 #incrementamos i
+	j for #regresamos al for
 	
 Accumulate:
-	add $t5, $t5, $t4 #sumamos todo en el acumulador
-	 jr $ra
+	add $t5, $t5, $t7  #sumamos todo en el acumulador
+	jr $ra #regresamos a donde nos invocaron
 	
 Exit:	
 
