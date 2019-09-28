3fe85616572200f53e165424ee48daf24005f9a1
Daniel Ruvalcaba <ie715472@iteso.mx>
2019-09-28 13:36:49 -0500

    Tarea 6 Fibonacci

--- Tarea_6.asm
+++ Tarea_6.asm
@@ -2,27 +2,40 @@
 
 .text
 
-	li $s0, 5 #s0 Sera N
-	li $s1, 0 # nuestro i
+	li $a0, 5 #s0 Sera N
+	addi $a0, $a0, -1
 	jal fibonacci
+	add $s0, $zero, $v0
 	j exit #salimos del programa
 	
 	fibonacci:
-	slti $t0, $s0, 2
-	beq $t0, $zero, loop #nos vamos a loop en caso de n>=2
-	addi $v0, $zero, 1 #cargamos 1 en caso de n=0 o n=1
+	slti $t0, $a0, 2
+	beq $t0, $zero, general #nos vamos a loop en caso de n>=2
+	add $v0, $zero, $s0
+	j endif
+	
+	general:
+	addi $sp, $sp, -12 #reservamos espacio en el stack
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	sw $t1, 8($sp)
+	
+	add $s0, $zero, $a0 
+	addi $a0, $s0, -1 #n-1
+	jal fibonacci
+	add $t1, $zero, $v0
+	
+	addi $a0, $s0, -2 #n-2
+	jal fibonacci
+	add $v0, $t1, $v0
+
+	
+	endif:
+	lw $ra, 0($sp)
+	lw $s0, 4($sp)
+	lw $t1, 8($sp)
+	addi $sp, $sp, 12
 	jr $ra
-	
-	loop:
-	addi $sp, $sp, -8 #reservamos espacio en el stack
-	subi $t1, $s0, 1 #guardamos n-1
-	subi $t2, $s0, 2 #guardamos n-2 
-	add $t1, $t1, $t2 #sumamos (n-1)+(n-2)
-	sw $ra, 4($sp) #guardamos la direccion de regreso
-	sw $t1, 0($sp) #guardamos 
-	addi $s0, $zero, -1 #decrementamos n
-	jal
-	
-	
+
 	exit:
 	
